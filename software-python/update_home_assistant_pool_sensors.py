65a862e79e047fbdbf3aa85871729a8f9c649844
Matt Sullivan <1115325+matt-sullivan@users.noreply.github.com>
2021-09-05 01:49:56 +1000

    Update python scripts to use mqtt username & password and wait for publish to send to the broker

--- software-python/update_home_assistant_pool_sensors.py
+++ software-python/update_home_assistant_pool_sensors.py
@@ -7,6 +7,7 @@ import read_pool_sensors as pool_sensors
 import dataclasses
 import json
 import logging
+import config
 
 BROKER="homeassistant.local"
 UNIQUE_ID_PREFIX = "b827eb771bbc_pool_controller_"
@@ -51,39 +52,46 @@ class DecimalEncoder(json.JSONEncoder):
             return f'{obj.normalize():f}'  # using normalize() gets rid of trailing 0s, using ':f' prevents scientific notation
         return super().encode(obj)
 
-def update_sensor_config(sensor_name, value_field, unit_of_measurement, device_class):
+def init_mqtt():
     mqtt_client = mqtt.Client()
+    mqtt_client.username_pw_set(config.mqtt_username, config.mqtt_password)
     mqtt_client.connect(BROKER)
+    mqtt_client.loop_start()
+    return mqtt_client
 
+def update_sensor_config(mqtt_client, sensor_name, value_field, unit_of_measurement, device_class):
     topic = discovery_topic(sensor_name)
     message = discovery_message(sensor_name, value_field, unit_of_measurement, device_class)
     logger.info(f"Sending discovery topic={topic}, message={message}")
-    mqtt_client.publish(topic, message)
+    # Retaining config messages should be useful if home assistant restarts, but the flag to the
+    # following method doesn't seem to work (HA mqtt debugging still shows false.)
+    message_info = mqtt_client.publish(topic, message, qos=1, retain=True)
+    message_info.wait_for_publish()
 
-def update_sensor_values(sensor_name, values):
-    mqtt_client = mqtt.Client()
-    mqtt_client.connect(BROKER)
-
+def update_sensor_values(mqtt_client, sensor_name, values):
     topic = status_topic(sensor_name)
     message = json.dumps(dataclasses.asdict(values), cls=DecimalEncoder)
     logger.info(f"Sending status topic={topic}, message={message}")
-    mqtt_client.publish(topic, message)
+    message_info = mqtt_client.publish(topic, message)
+    message_info.wait_for_publish()
 
-def update_all_sensor_configs():
-    update_sensor_config("pool_ph", "ph", None, None)
-    update_sensor_config("pool_orp", "orp", "mV", None)
-    update_sensor_config("pool_temp", "temperature", "°C", "temperature")
+def update_all_sensor_configs(mqtt_client):
+    update_sensor_config(mqtt_client, "pool_ph", "ph", None, None)
+    update_sensor_config(mqtt_client, "pool_orp", "orp", "mV", None)
+    update_sensor_config(mqtt_client, "pool_temp", "temperature", "°C", "temperature")
 
-def update_all_sensor_values():
-    update_sensor_values("pool_ph",pool_sensors.read_ph())
-    update_sensor_values("pool_orp", pool_sensors.read_orp())
-    update_sensor_values("pool_temp", pool_sensors.read_temp())
+def update_all_sensor_values(mqtt_client):
+    update_sensor_values(mqtt_client, "pool_ph",pool_sensors.read_ph())
+    update_sensor_values(mqtt_client, "pool_orp", pool_sensors.read_orp())
+    update_sensor_values(mqtt_client, "pool_temp", pool_sensors.read_temp())
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
+    mqtt_client = init_mqtt()
     if len(argv) > 1 and argv[1] == 'config' :
-        update_all_sensor_configs()
+        update_all_sensor_configs(mqtt_client)
     elif len(argv) > 1 and argv[1] == 'values':
-        update_all_sensor_values()
+        update_all_sensor_values(mqtt_client)
     else:
         print(f"Usage: {argv[0]} config|values")
+    mqtt_client.disconnect()
